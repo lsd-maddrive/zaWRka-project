3a9e6339da3ae694508250073ab1bbc64e474219
AlexSAU <alex_sau@etu.ru>
2019-03-01 19:42:35 +0300

    [wr8_soft] monocular SLAM launch prepared

--- wr8_software/prepare.sh
+++ wr8_software/prepare.sh
@@ -8,7 +8,9 @@ sudo apt install ros-kinetic-hector-mapping \
 					ros-kinetic-gazebo9-plugins \
 					ros-kinetic-map-server \
 					ros-kinetic-amcl \
-					ros-kinetic-laser-scan-matcher
+					ros-kinetic-laser-scan-matcher \
+					ros-kinetic-stereo-image-proc \
+					ros-kinetic-image-view
 pip install pygame
 
 cd scripts/graph_path; pyrcc5 -o resources.py my.qrc
