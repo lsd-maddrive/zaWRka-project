6386c6e447c61f9ef1578203f0da4153a635cf25
AlexSAU <alex_sau@etu.ru>
2019-03-02 13:16:43 +0300

    [wr8_firm/ROS] ROS new functions

--- controller_wr_driver/include/ros_protos.h
+++ controller_wr_driver/include/ros_protos.h
@@ -14,14 +14,26 @@ extern "C" {
 
 void ros_driver_init( tprio_t prio );
 void ros_driver_send_encoder_raw( int32_t value );
+
+/* value - Encoder rotation speed [rev/s] */
 void ros_driver_send_encoder_speed( float value );
+
 void ros_driver_send_steering( float steer_angle );
 /*
- * x - [meters]
- * y - [meters]
- * dir - [degree]
+ * x - Pose x coordinate [meters]
+ * y - Pose y coordinate [meters]
+ * dir - Direction angle [degree]
+ * vx - Transltaion speed [m/s]
+ * uz - Rotation speed [rad/s]
  */
-void ros_driver_send_pose( float x, float y, float dir );
+void ros_driver_send_pose( float x, float y, float dir, float vx, float uz );
+
+/*
+ * Cb returned arguments:
+ * 		speed - Speed task (-100;100) [%]
+ * 		steer - Steering task (-25;25) [deg]
+ */
+void ros_driver_set_control_cb( void (*cb_func)(float speed, float steer) );
 
 #ifdef __cplusplus
 }
