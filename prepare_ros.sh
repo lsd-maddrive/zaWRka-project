009c08b7b8b6e9964a2ed40156a08662dcdad096
KaiL4eK <darklexi494@gmail.com>
2019-03-16 09:25:26 +0300

    [wr8_soft] speech test

--- prepare_ros.sh
+++ prepare_ros.sh
@@ -33,41 +33,29 @@ sudo apt purge ros-$ROS_DISTRO-teb-local-planner*
 
 if [ "$ROS_DISTRO" = "kinetic" ]; then
 	sudo apt install ros-$ROS_DISTRO-hector-mapping \
-						ros-$ROS_DISTRO-move-base \
 						ros-$ROS_DISTRO-gmapping \
 						ros-$ROS_DISTRO-global-planner \
-						ros-$ROS_DISTRO-costmap-converter \
-						ros-$ROS_DISTRO-map-server \
-						ros-$ROS_DISTRO-amcl \
 						ros-$ROS_DISTRO-laser-scan-matcher \
-						ros-$ROS_DISTRO-stereo-image-proc \
-						ros-$ROS_DISTRO-image-view \
-						ros-$ROS_DISTRO-teleop-twist-keyboard \
-						ros-$ROS_DISTRO-interactive-markers \
-						ros-$ROS_DISTRO-usb-cam \
-						ros-$ROS_DISTRO-libg2o \
-						ros-$ROS_DISTRO-qt-build \
-						ros-$ROS_DISTRO-qt-gui* \
-						pyqt5-dev-tools
-else
-	sudo apt install ros-$ROS_DISTRO-move-base \
-						ros-$ROS_DISTRO-global-planner \
-						ros-$ROS_DISTRO-costmap-converter \
-						ros-$ROS_DISTRO-map-server \
-						ros-$ROS_DISTRO-amcl \
-						ros-$ROS_DISTRO-stereo-image-proc \
-						ros-$ROS_DISTRO-image-view \
-						ros-$ROS_DISTRO-teleop-twist-keyboard \
-						ros-$ROS_DISTRO-interactive-markers \
-						ros-$ROS_DISTRO-usb-cam \
-						ros-$ROS_DISTRO-libg2o \
-						ros-$ROS_DISTRO-qt-gui* \
-						pyqt5-dev-tools
+						ros-$ROS_DISTRO-qt-build
 fi
+
+sudo apt install ros-$ROS_DISTRO-move-base \
+					ros-$ROS_DISTRO-global-planner \
+					ros-$ROS_DISTRO-costmap-converter \
+					ros-$ROS_DISTRO-map-server \
+					ros-$ROS_DISTRO-amcl \
+					ros-$ROS_DISTRO-stereo-image-proc \
+					ros-$ROS_DISTRO-image-view \
+					ros-$ROS_DISTRO-interactive-markers \
+					ros-$ROS_DISTRO-usb-cam \
+					ros-$ROS_DISTRO-libg2o \
+					ros-$ROS_DISTRO-qt-gui*
+
+sudo apt install libespeak-dev pyqt5-dev-tools
 
 # ros-$ROS_DISTRO-gazebo9-plugins \
 
 # Dont`forget /opt/movidius/intel-caffe/python
 
-pip install pygame pyserial catkin-pkg rospkg empy defusedxml netifaces numpy
+pip install pygame pyserial catkin-pkg rospkg empy defusedxml netifaces numpy pyttsx3
 cd wr8_software/scripts/graph_path; pyrcc5 -o resources.py my.qrc
