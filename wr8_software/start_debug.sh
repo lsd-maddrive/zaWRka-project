4d730e548c3cfcb77903fd390c278ef33109c43e
Alexey <nine-4lex@mail.ru>
2019-04-15 12:26:47 +0300

    [ros] Initial for stereo

--- wr8_software/start_debug.sh
+++ wr8_software/start_debug.sh
@@ -2,10 +2,11 @@
 
 # stop_base.sh
 
-roslaunch wr8_software base_launch.launch   camera_s:=false \
+roslaunch wr8_software debug_launch.launch  camera_s:=false \
 											camera_r:=false \
 											solver:=false \
 											lidar:=false \
-											gui_server:=true \
+											gui_server:=false \
 											uc:=true \
+											stereo:true \
 											bag_record:=false
