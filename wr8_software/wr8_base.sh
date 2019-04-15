8c2a5757168df890b41ee624b041078e2a155606
Alexey <nine-4lex@mail.ru>
2019-04-15 12:27:41 +0300

    [ros] Just rename

---
+++ wr8_software/wr8_base.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# stop_base.sh
+
+roslaunch wr8_software base_launch.launch   camera_s:=false \
+											camera_r:=false \
+											solver:=true \
+											lidar:=true \
+											gui_server:=false \
+											uc:=true
