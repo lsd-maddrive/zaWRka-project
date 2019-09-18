63f7541666ae3ba06134e400b13f2eb8abc485c1
AlexSAU <alex_sau@etu.ru>
2019-09-18 09:33:29 +0300

    [ROS]: More packages for install

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -14,6 +14,8 @@ sudo apt install ros-$ROS_DISTRO-base-local-planner \
 					ros-$ROS_DISTRO-map-server \
 					ros-$ROS_DISTRO-amcl \
 					ros-$ROS_DISTRO-controller-interface \
+					ros-$ROS_DISTRO-realtime-tools \
+					ros-$ROS_DISTRO-urdf \
 					libsuitesparse-dev 
 
 # git -C wr8_gui_server/smart_vehicle_gui pull 	|| git -C wr8_gui_server clone https://github.com/lilSpeedwagon/smart_vehicle_gui.git
