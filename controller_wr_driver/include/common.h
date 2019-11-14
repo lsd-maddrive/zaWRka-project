1604f4f93a7a58880fff893c351f24ee2b138733
AlexSAU <alex_sau@etu.ru>
2019-11-14 10:22:07 +0300

    [uc] Mode set to master

--- controller_wr_driver/include/common.h
+++ controller_wr_driver/include/common.h
@@ -47,7 +47,7 @@ extern "C" {
 #define     PROGRAM_ROUTINE_TEST_ROS                    60
 #define     PROGRAM_ROUTINE_TEST_ROS_ADC_CALIB          61
 
-#define     MAIN_PROGRAM_ROUTINE                        PROGRAM_ROUTINE_TEST_RC
+#define     MAIN_PROGRAM_ROUTINE                        PROGRAM_ROUTINE_MASTER
 
 
 /*============================================================================*/
