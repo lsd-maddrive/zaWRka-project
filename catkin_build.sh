149d3634f2424c01e815bcc3771225deb873460c
Alexey <nine-4lex@mail.ru>
2019-11-28 16:14:14 +0300

    [ROS] Previous commit was for ROS removal + tests replacement -> now cleaning added

--- catkin_build.sh
+++ catkin_build.sh
@@ -8,3 +8,7 @@ touch hector_slam/hector_geotiff/CATKIN_IGNORE
 touch hector_slam/hector_geotiff_plugins/CATKIN_IGNORE
 
 catkin build -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O3" --env-cache
+
+# Clear to be ok for git
+rm hector_slam/hector_geotiff/CATKIN_IGNORE
+rm hector_slam/hector_geotiff_plugins/CATKIN_IGNORE
