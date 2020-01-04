13d3a2241df069d7ea80c6ca00f153c37237c758
AlexSAU <alex_sau@etu.ru>
2020-01-04 22:21:06 +0300

    [ROS] tests hided

--- wr8_software/scripts/action_cam_driver.py
+++ wr8_software/scripts/action_cam_driver.py
@@ -17,23 +17,31 @@ bridge = CvBridge()
 
 import numpy as np
 import cv2
+print(cv2.__version__)
+
 import time
 
 
-cap = cv2.VideoCapture('/dev/v4l/by-id/usb-USB_Developer_Android_20080411-video-index0')
+cap = cv2.VideoCapture('/dev/v4l/by-id/usb-KYE_Systems_Corp._USB_Camera_200901010001-video-index0')
 if cap is None or not cap.isOpened(): 
     rospy.logerr('Failed to open camera')
     exit(1)
 
-useFPS = False
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+print(cap.get(cv2.CAP_PROP_FPS))
+# cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
+
+print(cap.get(cv2.CAP_PROP_CONVERT_RGB))
+
+useFPS = True
+
 
 def talker():
 
     start_time, end_time, full_time = 0, 0, 0
     frame_cntr   = 0
     measure_time = 1
-
-    speed, steer = 1510, 1520
 
     # rate = rospy.Rate(30) # 1hz
     while not rospy.is_shutdown():
@@ -45,7 +53,7 @@ def talker():
             print('Failed to read frame')
             break
 
-        frame = cv2.resize(frame, (640, 480))
+        # frame = cv2.resize(frame, (640, 480))
 
         if useFPS:
             end_time = time.time()
@@ -58,20 +66,15 @@ def talker():
 
                 print('FPS: %g' % fps)
 
-        # cv2.imshow('frame', frame)
-        # if cv2.waitKey(30) == ord('q'):
-            # break
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
         msgImg.format = "jpeg"
         msgImg.data = np.array(cv2.imencode('.jpg', frame)[1]).tostring()
         img_pub_comp.publish(msgImg)
 
         img_pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
-
-        # for steer in range(1270, 1790, 10):
-            # rospy.loginfo('Speed: {} / Steer: {}'.format(speed, steer))
-            # sendControl( speed, steer )
-        # rate.sleep()
 
 if __name__ == '__main__':
     try:
