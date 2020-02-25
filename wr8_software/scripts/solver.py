49b3f4e00d7fe75b5ed22e5c1121677e253de0bd
ponomarevda <ponomarevda96@gmail.com>
2020-02-26 01:44:15 +0300

    [ROS main solver] little clean up

--- wr8_software/scripts/solver.py
+++ wr8_software/scripts/solver.py
@@ -1,25 +1,57 @@
 #!/usr/bin/env python
+import math
+from enum import Enum
+
 import rospy
 import tf
 import actionlib
-from geometry_msgs.msg import PoseStamped
-from nav_msgs.msg import Path
 from std_msgs.msg import UInt8
+from geometry_msgs.msg import PoseStamped, PolygonStamped, Point
+from nav_msgs.msg import Path
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from geometry_msgs.msg import PolygonStamped
-from geometry_msgs.msg import Point
-from car_parking.msg import Point2D
-from car_parking.msg import Points2D
-from car_parking.msg import Polygons
-from car_parking.msg import Statuses
+from car_parking.msg import Point2D, Points2D, Polygons, Statuses
 
-import math
 import numpy as np
 import pygame
-import time
-from enum import Enum
 
 from maze import MazePoint, PointDir, Maze
+
+
+# Params
+MAZE_TARGET_X = 1
+MAZE_TARGET_Y = 2
+START_X = 15
+START_Y = 19
+START_Z = 0 # 0 - right, 1.57 - up
+CELL_SZ = 2
+
+
+# Coordinate transform from MazePoint to MazePoint
+def maze_to_gz(p):
+    gz_x = p.x * CELL_SZ + 0.5*CELL_SZ
+    gz_y = p.y * CELL_SZ + 0.5*CELL_SZ
+    return MazePoint(gz_x, gz_y)
+
+def gz_to_maze(p):
+    maze_x = round((p.x - 0.5*CELL_SZ) / CELL_SZ)
+    maze_y = round((p.y - 0.5*CELL_SZ) / CELL_SZ)
+    return MazePoint(maze_x, maze_y)
+
+def gz_to_map(p):
+    map_x = (p.x - START_X) * math.cos(-START_Z) - (p.y - START_Y) * math.sin(-START_Z)
+    map_y = (p.x - START_X) * math.sin(-START_Z) + (p.y - START_Y) * math.cos(-START_Z)
+    return MazePoint(map_x, map_y)
+
+def map_to_gz(p):
+    gz_x = START_X + p.x * math.cos(START_Z) - p.y * math.sin(START_Z)
+    gz_y = START_Y + p.x * math.sin(START_Z) + p.y * math.cos(START_Z)
+    return MazePoint(gz_x, gz_y)
+
+def maze_to_map(p):
+    return gz_to_map(maze_to_gz(p))
+
+def map_to_maze(p):
+    return gz_to_maze(map_to_gz(p))
 
 
 # Constants
@@ -37,98 +69,97 @@ class State(Enum):
     PARKING_PROCESS = 7
     PARKING_REACHED = 8
 
-# Params
-MAZE_TARGET_X = 1
-MAZE_TARGET_Y = 2
-OFFSET_X = 15
-OFFSET_Y = 19
-OFFSET_Z = 3.14 # 1.57 - up, 3.14 - left
-CELL_SZ = 2
+NODE_NAME = "solver_node"
+HW_SUB_TOPIC = "hardware_status"
+
+FRAME_ID = "map"
+PATH_PUB_NAME = "path"
+
+POLY_PUB_TOPIC = "parking_polygones"
+RVIZ_PUB_TOPIC = "parking_polygones_visualization_"
+STATUS_SUB_TOPIC = "parking_status"
+CMD_PUB_TOPIC = "parking_cmd"
+
+PARKING_GOALS = tuple((gz_to_map(MazePoint(5, 14)),
+                       gz_to_map(MazePoint(5, 16)),
+                       gz_to_map(MazePoint(5, 18))))
+PARKING_WAITING_GOAL = gz_to_map(MazePoint(3, 16))
+PARKING_TOLERANCE = 70
 
 
-# Coordinate transform from MazePoint to MazePoint
-def maze_to_gz(p):
-    gz_x = p.x * CELL_SZ + 0.5*CELL_SZ
-    gz_y = p.y * CELL_SZ + 0.5*CELL_SZ
-    return MazePoint(gz_x, gz_y)
-
-def gz_to_maze(p):
-    maze_x = round((p.x - 0.5*CELL_SZ) / CELL_SZ)
-    maze_y = round((p.y - 0.5*CELL_SZ) / CELL_SZ)
-    return MazePoint(maze_x, maze_y)
-
-def gz_to_map(p):
-    map_x = (p.x - OFFSET_X) * math.cos(-OFFSET_Z) - (p.y - OFFSET_Y) * math.sin(-OFFSET_Z)
-    map_y = (p.x - OFFSET_X) * math.sin(-OFFSET_Z) + (p.y - OFFSET_Y) * math.cos(-OFFSET_Z)
-    return MazePoint(map_x, map_y)
-
-def map_to_gz(p):
-    gz_x = OFFSET_X + p.x * math.cos(OFFSET_Z) - p.y * math.sin(OFFSET_Z)
-    gz_y = OFFSET_Y + p.x * math.sin(OFFSET_Z) + p.y * math.cos(OFFSET_Z)
-    return MazePoint(gz_x, gz_y)
-
-def maze_to_map(p):
-    return gz_to_map(maze_to_gz(p))
-
-def map_to_maze(p):
-    return gz_to_maze(map_to_gz(p))
-
-
-class MainSolver:
+class MainSolver(object):
     def __init__(self):
-        MainSolver.hardware_status = False
-        NODE_NAME = "solver_node"
-        HARDWARE_SUB_TOPIC = "hardware_status"
-
         rospy.init_node(NODE_NAME, log_level=rospy.DEBUG)
-        self.HARDWARE_SUB = rospy.Subscriber(HARDWARE_SUB_TOPIC, UInt8, MainSolver._hardware_cb, queue_size=10)
-        self.GOAL_CLIENT = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-        self.GOAL_CLIENT.wait_for_server()
-        self.POSE_LISTENER = tf.TransformListener()
-        time.sleep(1)
+        MainSolver.hardware_status = False
+        self.hw_sub = rospy.Subscriber(HW_SUB_TOPIC, UInt8, MainSolver._hardware_cb, queue_size=10)
+        self.goal_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.goal_client.wait_for_server()
+        self.pose_listener = tf.TransformListener()
+        rospy.sleep(1) # need pause before first publish but maze consructor needs crnt pose
         crnt = self._get_maze_current_pose()
         self.maze = MazeSolver(crnt)
         self.parking = ParkingSolver()
 
     def process(self):
         crnt = self._get_maze_current_pose()
-        state = determinate_state(crnt)
+        state = MainSolver._determinate_state(crnt)
         if state == State.MAZE_PROCESS:
+            self.parking.stop_work()
             goal_point = self.maze.process(crnt)
         elif state == State.SPEED_PROCESS:
+            self.parking.stop_work()
             goal_point = process_speed()
         elif state == State.PARKING_PROCESS:
+            self.parking.start_work()
             goal_point = self.parking.process()
         else:
             self._send_stop_cmd()
+            self.parking.stop_work()
             return
         self._send_goal(goal_point)
         self.parking.publish_to_rviz()
-        rospy.loginfo("%s: crnt pose (maze) = %s, glob goal(map) = %s", str(state), crnt, goal_point)
+        rospy.loginfo("%s: crnt pose (maze) = %s, glob goal(map) = %s",
+                      str(state), crnt, goal_point)
+
+    @staticmethod
+    def _determinate_state(crnt):
+        """
+        crnt - maze current pose
+        """
+        state = State
+        if MainSolver.hardware_status is False or crnt is None:
+            state = State.IDLE
+        elif crnt.x >= 3 or (crnt.x >= 2 and crnt.y <= 5):
+            state = State.MAZE_PROCESS
+        elif crnt.y <= 5:
+            state = State.SPEED_PROCESS
+        elif crnt.y > 5:
+            state = State.PARKING_PROCESS
+        return state
 
     def _get_maze_current_pose(self):
-        trans = self.POSE_LISTENER.lookupTransform('/map', '/base_footprint', rospy.Time(0))
+        trans = self.pose_listener.lookupTransform('/map', '/base_footprint', rospy.Time(0))
         point = map_to_maze(MazePoint(trans[0][0], trans[0][1]))
         return point
 
     def _send_goal(self, point):
         goal = MoveBaseGoal()
-        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.header.frame_id = FRAME_ID
         goal.target_pose.header.stamp = rospy.Time.now()#rospy.get_rostime()
         goal.target_pose.pose.position.x = point.x
         goal.target_pose.pose.position.y = point.y
         goal.target_pose.pose.orientation.w = 1.0
 
-        self.GOAL_CLIENT.send_goal(goal)
-        #wait = self.GOAL_CLIENT.wait_for_result(rospy.Duration.from_sec(5.0))
+        self.goal_client.send_goal(goal)
+        #wait = self.goal_client.wait_for_result(rospy.Duration.from_sec(5.0))
         #if not wait:
         #    rospy.logerr("Action server is not available!")
         #    #rospy.signal_shutdown("Action server is not available!")
         #else:
-        #    print self.GOAL_CLIENT.get_result()
+        #    print self.goal_client.get_result()
 
     def _send_stop_cmd(self):
-        self.GOAL_CLIENT.cancel_goal()
+        self.goal_client.cancel_goal()
 
     @staticmethod
     def _hardware_cb(msg):
@@ -138,11 +169,8 @@ class MainSolver:
         elif msg.data == 1:
             MainSolver.hardware_status = True
 
-class MazeSolver:
+class MazeSolver(object):
     def __init__(self, initial_pose):
-        MazeSolver.FRAME_ID = "map"
-        PATH_PUB_NAME = "path"
-
         MazeSolver.PATH_PUB = rospy.Publisher(PATH_PUB_NAME, Path, queue_size=5)
 
         structure = [[8, 8, 8, 0, 0, 0, 0, 0, 0, 0],
@@ -180,7 +208,7 @@ class MazeSolver:
                 rospy.logdebug("New path:")
                 for node in path:
                     rospy.logdebug("- %s", node)
-                
+
                 self.maze.next_local_target()
             goal_point = maze_to_map(path[-1].coord)
         return goal_point
@@ -189,11 +217,11 @@ class MazeSolver:
     def _pub_path(nodes):
         path = Path()
         path.header.seq = 1
-        path.header.frame_id = MazeSolver.FRAME_ID
+        path.header.frame_id = FRAME_ID
         for i in range(0, len(nodes)):
             pose = PoseStamped()
             pose.header.seq = 1
-            pose.header.frame_id = MazeSolver.FRAME_ID
+            pose.header.frame_id = FRAME_ID
 
             point = maze_to_map(nodes[i].coord)
             pose.pose.position.x = point.x
@@ -203,37 +231,40 @@ class MazeSolver:
         MazeSolver.PATH_PUB.publish(path)
 
 
-class ParkingSolver:
+class ParkingSolver(object):
     def __init__(self):
-        POLY_PUB_TOPIC = "parking_polygones"
-        RVIZ_PUB_TOPIC = "parking_polygones_visualization_"
-        STATUS_SUB_TOPIC = "parking_status"
-        CMD_TOPIC = "parking_cmd"
-
         rospy.Subscriber(STATUS_SUB_TOPIC, Statuses, self._status_cb, queue_size=10)
-        self.PUB_TO_PARKING = rospy.Publisher(POLY_PUB_TOPIC, Polygons, queue_size=10)
-        self.PUB_TO_RVIZ = list()
+        self.pub_to_parking = rospy.Publisher(POLY_PUB_TOPIC, Polygons, queue_size=10)
+        self.pub_to_cmd = rospy.Publisher(CMD_PUB_TOPIC, UInt8, queue_size=10)
+        self.pub_to_rviz = list()
         for i in range(3):
             new_pub = rospy.Publisher(RVIZ_PUB_TOPIC + str(i + 1), PolygonStamped, queue_size=10)
-            self.PUB_TO_RVIZ.append(new_pub)
+            self.pub_to_rviz.append(new_pub)
 
         self.parking_polygones, self.rviz_polygones = ParkingSolver._create_polygones()
         self.fullness = list([-1, -1, -1])
 
+    def start_work(self):
+        self.pub_to_cmd.publish(1)
+
+    def stop_work(self):
+        self.pub_to_cmd.publish(0)
+
     def publish_to_rviz(self):
         for i in range(3):
-            self.PUB_TO_RVIZ[i].publish(self.rviz_polygones[i])       
+            self.pub_to_rviz[i].publish(self.rviz_polygones[i])
 
     def process(self):
-        self.PUB_TO_PARKING.publish(self.parking_polygones)
-        
-        goal = gz_to_map(MazePoint(3, 16))
-        if self.fullness[0] >= 0 and self.fullness[0] <= 70:
-            goal = gz_to_map(MazePoint(5, 14))
-        elif self.fullness[1] >= 0 and self.fullness[1] <= 70:
-            goal = gz_to_map(MazePoint(5, 16))
-        elif self.fullness[2] >= 0 and self.fullness[2] <= 70:
-            goal = gz_to_map(MazePoint(5, 18))
+        self.pub_to_parking.publish(self.parking_polygones)
+
+        goal = PARKING_WAITING_GOAL
+        if len(self.fullness) == 3:
+            if self.fullness[0] >= 0 and self.fullness[0] <= PARKING_TOLERANCE:
+                goal = PARKING_GOALS[0]
+            elif self.fullness[1] >= 0 and self.fullness[1] <= PARKING_TOLERANCE:
+                goal = PARKING_GOALS[1]
+            elif self.fullness[2] >= 0 and self.fullness[2] <= PARKING_TOLERANCE:
+                goal = PARKING_GOALS[2]
         return goal
 
     @staticmethod
@@ -250,32 +281,30 @@ class ParkingSolver:
             map_polygons.append(ParkingSolver._gz_to_map_for_polygon(gz_polygons[i]))
             polygons.append(ParkingSolver._create_polygon(map_polygons[-1]))
             polygons_stamped.append(ParkingSolver._create_polygon_stamped(map_polygons[-1]))
-
         parking_polygones = Polygons()
         parking_polygones.polygons = tuple(polygons)
         return [parking_polygones, polygons_stamped]
 
     @staticmethod
     def _create_polygon(polygon):
-        p1 = Point2D(); p1.x = polygon[0][0]; p1.y = polygon[0][1]
-        p2 = Point2D(); p2.x = polygon[1][0]; p2.y = polygon[1][1]
-        p3 = Point2D(); p3.x = polygon[2][0]; p3.y = polygon[2][1]
-        p4 = Point2D(); p4.x = polygon[3][0]; p4.y = polygon[3][1]
+        points2D = [Point2D(), Point2D(), Point2D(), Point2D()]
+        for i in range(4):
+            points2D[i].x = polygon[i][0]
+            points2D[i].y = polygon[i][1]
         poly = Points2D()
-        poly.points = (p1, p2, p3, p4)
+        poly.points = (points2D)
         return poly
 
     @staticmethod
     def _create_polygon_stamped(polygon):
-        p1 = Point(); p1.x = polygon[0][0]; p1.y = polygon[0][1]
-        p2 = Point(); p2.x = polygon[1][0]; p2.y = polygon[1][1]
-        p3 = Point(); p3.x = polygon[2][0]; p3.y = polygon[2][1]
-        p4 = Point(); p4.x = polygon[3][0]; p4.y = polygon[3][1]
-
+        points = [Point(), Point(), Point(), Point()]
+        for i in range(4):
+            points[i].x = polygon[i][0]
+            points[i].y = polygon[i][1]
         polygons_stumped = PolygonStamped()
         polygons_stumped.header.stamp = rospy.get_rostime()
-        polygons_stumped.header.frame_id = 'map'
-        polygons_stumped.polygon.points = tuple((p1, p2, p3, p4))
+        polygons_stumped.header.frame_id = FRAME_ID
+        polygons_stumped.polygon.points = tuple(points)
         return polygons_stumped
 
     @staticmethod
@@ -303,26 +332,9 @@ def process_speed():
     return maze_to_map(MazePoint(1, 7))
 
 
-def determinate_state(crnt):
-    """
-    crnt - maze current pose
-    """
-    state = State
-    if MainSolver.hardware_status is False or crnt is None:
-        state = State.IDLE
-    elif crnt.x >= 3 or (crnt.x >= 2 and crnt.y <= 5):
-        state = State.MAZE_PROCESS
-    elif crnt.y <= 5:
-        state = State.SPEED_PROCESS
-    elif crnt.y >= 6:
-        state = State.PARKING_PROCESS
-    return state
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     solver = MainSolver()
-    rate = rospy.Rate(1)
+    RATE = rospy.Rate(1)
     while not rospy.is_shutdown():
         solver.process()
-        rate.sleep()
-
+        RATE.sleep()
