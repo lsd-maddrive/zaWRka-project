c9148746aed5e196853ff2d16e26a1055b48a425
ponomarevda <ponomarevda96@gmail.com>
2020-03-04 15:38:17 +0300

    [ROS main solver] improve test

--- wr8_software/scripts/test_solver.py
+++ wr8_software/scripts/test_solver.py
@@ -10,36 +10,79 @@ from std_msgs.msg import UInt8
 from maze import MazePoint
 from solver import map_to_maze, TF_SUB_TOPIC, WL_SUB_TOPIC, SIGN_SUB_TOPIC, TFColor, SignType
 
+
 def get_cur_maze_pose(pose_listener):
-    trans = pose_listener.lookupTransform('/map', '/base_footprint', rospy.Time(0))
-    point = map_to_maze(MazePoint(trans[0][0], trans[0][1]))
+    try:
+        trans = pose_listener.lookupTransform('/map', '/base_footprint', rospy.Time(0))
+        point = map_to_maze(MazePoint(trans[0][0], trans[0][1]))
+    except:
+        point = None
     return point
 
-class SignStop(object):
+class Sign(object):
+    previous_sign_type = SignType.NO_SIGN
+    sign_type = SignType.NO_SIGN
+    def __init__(self):
+        Sign.sign_type = SignType.NO_SIGN
+
+class SignStop(Sign):
     def do(self):
-        sign_pub.publish(SignType.STOP.value)
-        print "SignStop activated"
+        Sign.sign_type = SignType.STOP
+
+class SignForward(Sign):
+    def do(self):
+        Sign.sign_type = SignType.ONLY_FORWARD
+
+class SignRight(Sign):
+    def do(self):
+        Sign.sign_type = SignType.ONLY_RIGHT
+
+class SignLeft(Sign):
+    def do(self):
+        Sign.sign_type = SignType.ONLY_LEFT
+
+class SignForwardOrRight(Sign):
+    def do(self):
+        Sign.sign_type = SignType.FORWARD_OR_RIGHT
+
+class SignForwardOrLeft(Sign):
+    def do(self):
+        Sign.sign_type = SignType.FORWARD_OR_LEFT
 
 class TF(object):
-    def do(self):
-        print "TF activated: RED for 15 sec"
-        tf_pub.publish(TFColor.RED.value)
-        wl_pub.publish(1)
-        sleep(15)
-        wl_pub.publish(0)
-        tf_pub.publish(TFColor.GREEN.value)
+    tf_color = TFColor.UNKNOWN
+    wl_status = 0
+    previous_tf_color = TFColor.UNKNOWN
+    previous_wl_status = 0
+
+    def do(self, time = 7.5):
+        TF.tf_color = TFColor.RED
+        TF.wl_status = 1
+        rospy.Timer(rospy.Duration(time), self._change_tf_and_wl_cb, oneshot=True)
+
+    def _change_tf_and_wl_cb(self, event):
+        TF.tf_color = TFColor.GREEN
+        TF.wl_status = 0
 
 
 class Test(object):
     def __init__(self, point, obj):
         self.point = point
-        self.status = False
+        self.is_it_called_before = False
+        self.is_it_missed_before = False
         self.obj = obj
 
     def process(self, cur_maze_pose):
-        if self.point == cur_maze_pose and self.status == False:
+        if cur_maze_pose is None:
+            return
+
+        if self.point == cur_maze_pose and self.is_it_missed_before == False:
+            self.is_it_missed_before = True
+            return
+
+        if self.point == cur_maze_pose and self.is_it_called_before == False:
             self.obj.do()
-            self.status = True
+            self.is_it_called_before = True
 
 
 rospy.init_node("test_solver", log_level=rospy.DEBUG)
@@ -54,14 +97,38 @@ if __name__ == "__main__":
     tests = list()
     tests.append(Test(MazePoint(9, 8), TF()))
     tests.append(Test(MazePoint(9, 7), SignStop()))
-    print "go"
-    while not rospy.is_shutdown():
-        wl_pub.publish(0)
-        tf_pub.publish(TFColor.UNKNOWN.value)
-        sign_pub.publish(SignType.NO_SIGN.value)
+    tests.append(Test(MazePoint(7, 7), SignRight()))
+    tests.append(Test(MazePoint(3, 8), SignForwardOrRight()))
+    tests.append(Test(MazePoint(3, 5), SignStop()))
+    tests.append(Test(MazePoint(5, 4), SignForward()))
+    tests.append(Test(MazePoint(8, 4), SignRight()))
+    tests.append(Test(MazePoint(9, 3), SignForwardOrLeft()))
+    tests.append(Test(MazePoint(9, 1), TF()))
+    tests.append(Test(MazePoint(5, 0), TF()))
+    tests.append(Test(MazePoint(4, 0), TF()))
+    tests.append(Test(MazePoint(5, 2), TF()))
+    tests.append(Test(MazePoint(4, 2), TF()))
 
+    while not rospy.is_shutdown():
         cur_maze_point = get_cur_maze_pose(pose_listener)
-        print cur_maze_point
+        Sign.sign_type = SignType.NO_SIGN
+        
         for test in tests:
             test.process(cur_maze_point)
-        sleep(3)
+
+        if TF.previous_wl_status != TF.wl_status:
+            TF.previous_wl_status = TF.wl_status
+            wl_pub.publish(TF.wl_status)
+            rospy.logdebug("White line set to %d", TF.wl_status)
+
+        if TF.previous_tf_color != TF.tf_color:
+            TF.previous_tf_color = TF.tf_color
+            tf_pub.publish(TF.tf_color.value)
+            rospy.logdebug("TF set to %s", str(TF.tf_color))
+            
+        if Sign.previous_sign_type != Sign.sign_type:
+            Sign.previous_sign_type = Sign.sign_type
+            sign_pub.publish(Sign.sign_type.value)
+            rospy.logdebug("Sign set to %s", str(Sign.sign_type))
+
+        rospy.sleep(1)
