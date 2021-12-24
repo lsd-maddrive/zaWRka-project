0a9222577b5eeea9f5ab488374d8c0e866052b84
ponomarevda <ponomarevda96@gmail.com>
2021-12-24 19:40:44 +0300

    make world_creator as disctinct repo

--- world_creator/start_creator.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(rospack find world_creator)/models
-
-python3 scripts/world_creator.py $@
--- world_creator/start_gz.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-GAZEBO_MODEL_PATH=`pwd`/models:$GAZEBO_MODEL_PATH gazebo $1
