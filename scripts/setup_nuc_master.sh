74e8ee06b72f0c3e165c0c526612df52cf942bdd
DepartmentNotebookUser <dep-nb-user@mail.ru>
2022-02-19 18:04:20 +0300

    work on rgbd odometry

--- scripts/setup_nuc_master.sh
+++ scripts/setup_nuc_master.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export ROS_HOSTNAME=$(hostname).local
+export ROS_HOSTNAME=192.168.1.36
 export ROS_MASTER_URI=http://zawrka.local:11311
-# export ROS_MASTER_URI=http://10.139.1.139:11311
+export ROS_MASTER_URI=http://192.168.1.37:11311
 
 echo "Master: $ROS_MASTER_URI / Hostname: $ROS_HOSTNAME"
