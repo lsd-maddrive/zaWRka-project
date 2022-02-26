6247a7ee344575d09d17db9f6cded4f948fde34a
DepartmentNotebookUser <dep-nb-user@mail.ru>
2022-02-26 18:33:50 +0300

    cup can't reach

--- scripts/setup_nuc_master.sh
+++ scripts/setup_nuc_master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export ROS_HOSTNAME=$(hostname).local
-export ROS_HOSTNAME=192.168.1.36
-export ROS_MASTER_URI=http://zawrka.local:11311
-export ROS_MASTER_URI=http://192.168.1.37:11311
+export ROS_HOSTNAME=192.168.1.39
+# export ROS_MASTER_URI=http://zawrka.local:11311
+export ROS_MASTER_URI=http://192.168.1.40:11311
 
 echo "Master: $ROS_MASTER_URI / Hostname: $ROS_HOSTNAME"
