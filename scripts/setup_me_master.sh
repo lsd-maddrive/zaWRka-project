b735ebfcdeea4d3b8d0f9a43a87c56f36868fa23
katota <katota@gmail.com>
2023-03-06 16:52:44 +0300

    init 2dslam

--- scripts/setup_me_master.sh
+++ scripts/setup_me_master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ROS_HOSTNAME=$(hostname).local
-export ROS_HOSTNAME=10.139.1.139
+export ROS_HOSTNAME=192.168.88.95
 export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
 
 echo "Master: $ROS_MASTER_URI / Hostname: $ROS_HOSTNAME"
--- scripts/setup_nuc_master.sh
+++ scripts/setup_nuc_master.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export ROS_HOSTNAME=$(hostname).local
-# export ROS_MASTER_URI=http://zawrka.local:11311
-export ROS_MASTER_URI=http://10.139.1.139:11311
+export ROS_MASTER_URI=http://192.168.1.95:11311
 
 echo "Master: $ROS_MASTER_URI / Hostname: $ROS_HOSTNAME"
