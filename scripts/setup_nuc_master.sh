1ee748cee30b9a3c9f9f6a5bfed0210d410b61e1
katota <katota@gmail.com>
2023-03-07 17:41:40 +0300

    added keyboard control

--- scripts/setup_nuc_master.sh
+++ scripts/setup_nuc_master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export ROS_HOSTNAME=$(hostname).local
-export ROS_MASTER_URI=http://192.168.1.95:11311
+export ROS_MASTER_URI=http://192.168.88.95:11311
 
 echo "Master: $ROS_MASTER_URI / Hostname: $ROS_HOSTNAME"
